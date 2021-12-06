621828a53b2eb427399c168997e803b349204717
Martin Giura <martin.giura@gmail.com>
2021-12-06 16:45:01 -0600

    Tailwind

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@
  * COMPONENTES
  * 1. IMPORTACIONES
  */
-
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -23,14 +22,14 @@ function App() {
   // HTML + JS = JSX
   return (
     <div className="App">
+      <Header nombre="Mike" ciudad="Veracruz" />
       <p>Hola mundo</p>
       <p>Mike Nieva</p>
       <p>{12 * 12}</p>
       <p>{bootcamp}</p>
 
-      <Header nombre="Mike" ciudad="Veracruz" />
-      <Header nombre="Sam" ciudad="EdoMex" />
-      <Header nombre="K'onhin" ciudad="Hidalgo" />
+      {/* <Header nombre="Sam" ciudad="EdoMex" />
+       <Header nombre="K'onhin" ciudad="Hidalgo" /> */}
 
       <hr />
 
