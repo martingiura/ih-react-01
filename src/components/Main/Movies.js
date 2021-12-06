e07746808355b9270288bf1cadd880dc759f32e9
Martin Giura <martin.giura@gmail.com>
2021-12-06 13:07:34 -0600

    estructura base

---
+++ src/components/Main/Movies.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Movies(props) {
+  const list = props.list;
+
+  return (
+    <>
+      {list.map((e, index) => {
+        return (
+          <article key={index}>
+            <h1>{e.nombre}</h1>
+            <p>El género es: {e.genero}</p>
+          </article>
+        );
+      })}
+    </>
+  );
+}
